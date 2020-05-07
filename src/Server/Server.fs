64ea8e6d7f55e163f2b063af748cccb2a3c01c6f
Isaac Abraham <isaac.abraham@gmail.com>
2020-05-07 14:12:39 +0200

    Change to localhost

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -8,7 +8,7 @@ let webApp = router {
 }
 
 let app = application {
-    url "http://0.0.0.0:8085/"
+    url "http://localhost:8085"
     use_router webApp
     memory_cache
     use_static (Path.GetFullPath "../Client/public")
